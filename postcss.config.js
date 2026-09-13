1947284797bee5ae64c267f1e72632e664263c1c
YJPL <26725821+YJPL@users.noreply.github.com>
2026-09-13 21:38:29 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
   require('postcss-import'),
-  require('postcss-nested'),
+  require('postcss-nested').default, 
   require('postcss-preset-env'),
   require('autoprefixer')
   ]
